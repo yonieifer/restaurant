9e3c2629cbd8d249f93490b23c1088b4c2c46bee
Yonatan Eiferman <yonieifer@gmail.com>
2026-07-12 20:54:35 +0300

    add action: update status

--- router/orderRouter.js
+++ router/orderRouter.js
@@ -1,50 +1,73 @@
-import express from "express"
-import { writeData, readData } from "../data/dataAccess.js"
-import validateID from "../middlewares/validateID.js"
+import express from "express";
+import { writeData, readData } from "../data/dataAccess.js";
+import validateID from "../middlewares/validateID.js";
 
-const router = express.Router()
+const router = express.Router();
 
 router.post("/", async (req, res) => {
-    const {customer, table, dishesList} = req.body
-    const allOrders = await readData()
+    const { customer, table, dishesList } = req.body;
+    const allOrders = await readData();
     const newOrder = {
-        id: Math.max(0, ...allOrders.map(o => o.id)) + 1,
+        id: Math.max(0, ...allOrders.map((o) => o.id)) + 1,
         customer: customer,
         table: table,
         dishesList: dishesList,
-        status: "NEW"
-    }
-    await writeData(newOrder)
-    res.status(201).send("New order created")
-})
+        status: "NEW",
+    };
+    await writeData(newOrder);
+    res.status(201).send("New order created");
+});
 
 router.get("/", async (req, res) => {
-    const allOrders = await readData()
-    res.send(allOrders)
-})
+    const allOrders = await readData();
+    res.send(allOrders);
+});
 
 router.get("/:id", validateID, (req, res) => {
-    const order = req.order
-    res.send(order)
-})
+    const order = req.order;
+    res.send(order);
+});
 
 router.put("/:id", validateID, (req, res) => {
-    const {customer, table, dishesList} = req.body
+    const { customer, table, dishesList } = req.body;
     if (!customer || !table || !dishesList) {
-        return res.status(400).send("body field missing")
+        return res.status(400).send("body field missing");
     }
-    const id = req.params.id
-    const allOrders = readData()
-    const order = allOrders.find(order => order.id === +id)
-    Object.assign(order, {customer, table, dishesList}) 
-    writeData(allOrders)
+    const id = req.params.id;
+    const allOrders = readData();
+    const order = allOrders.find((order) => order.id === +id);
+    Object.assign(order, { customer, table, dishesList });
+    writeData(allOrders);
 
-    res.send(`Order ${id} updated`)
-})
+    res.send(`Order ${id} updated`);
+});
 
-// router.delete("/:id")
+router.delete("/:id", validateID, async (req, res) => {
+    const id = req.params.id;
+    const allOrders = await readData();
+    const updatedOrders = allOrders.filter((order) => order.id !== +id);
+    await writeData(updatedOrders);
+    res.send(`order ${id} deleted`);
+});
 
-// router.patch("/:id/status")
+router.patch("/:id/status", validateID, async (req, res) => {
+    const id = req.params.id;
+    const newStatus = req.body.status;
+    const allOrders = await readData();
+    const order = allOrders.find((order) => order.id === +id);
+    const oldStatus = order.status;
+    if (
+        (newStatus === "CANCELLED" &&
+            ["NEW", "PREPARING"].includes(oldStatus)) ||
+        (oldStatus === "NEW" && newStatus === "PREPARING") ||
+        (oldStatus === "PREPARING" && newStatus === "READY") ||
+        (oldStatus === "READY" && newStatus === "DELIVERED")
+    ) {
+        order.status = newStatus
+        await writeData(allOrders)
+        return res.send("updated")
+    }
+    res.status(400).send("not authorized status")
+});
 
-
-export default router
+export default router;
