68c2418218135ecc8b7ce86d572e44a04d3a3bd5
Yonatan Eiferman <yonieifer@gmail.com>
2026-07-12 16:23:51 +0300

    fix data path

--- data/dataAccess.js
+++ data/dataAccess.js
@@ -2,10 +2,10 @@ import fs from "fs/promises"
 
 
 export const readData = async () => {
-    const data = await fs.readFile("./orders.json", "utf8")
+    const data = await fs.readFile("./data/orders.json", "utf8")
     return JSON.parse(data)
 }
 
 export const writeData = async (data) => {
-    fs.writeFile("./orders.json", JSON.stringify(data))
+    fs.writeFile("./data/orders.json", JSON.stringify(data, null, 2))
 }
