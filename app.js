644adbc9c0d94854056ff09883f14d5de64c5ba4
Yonatan Eiferman <yonieifer@gmail.com>
2026-07-12 15:53:27 +0300

    add logger middleware

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 import express from "express"
+import router from "./router/orderRouter"
+import logger from "./middlewares/logger.js" 
 
 const app = express()
 
 app.use(express.json())
 
+app.use(logger)
+
+app.use("/order", router)
